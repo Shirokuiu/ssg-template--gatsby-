d900a053cb9011a8f43810c2bcc84bb9749fbe44
pavel <shirokuiuna@mail.ru>
2022-05-14 23:24:57 +0300

    Добавил возможность инлайновой вставки обычной svg картинки

--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -2,6 +2,8 @@ import { Link } from 'gatsby';
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
+import Icon from '@assets/img/atention.svg';
+
 function TestPage() {
   return (
     <ul>
@@ -10,6 +12,9 @@ function TestPage() {
       </li>
       <li>
         <StaticImage placeholder="blurred" src="../assets/img/icon.png" alt="test" />
+      </li>
+      <li>
+        <Icon className="svg-icon" style={{ width: '30px' }} />
       </li>
       <li>
         <Link to="/">Go home</Link>

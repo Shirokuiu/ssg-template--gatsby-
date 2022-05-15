1119048449a6d4a3da2ab08a043823677f0c8d26
pavel <shirokuiuna@mail.ru>
2022-05-15 22:43:12 +0300

    Добавил глобальное хранилище состояния - Redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,18 @@ import { Link } from 'gatsby';
 
 import PageLayout from '@components/shared/page-layout/page-layout';
 import CenterLayout from '@components/shared/center-layout/center-layout';
+import { useAppDispatch, useAppSelector } from '@hooks/index';
+import { getTitle } from '@store/index-page-process/selectors';
+import { clearState } from '@store/index-page-process/reducer/index-page-process';
 
 function IndexPage() {
+  const title = useAppSelector(getTitle);
+  const dispatch = useAppDispatch();
+
+  const handleButtonClick = () => {
+    dispatch(clearState());
+  };
+
   return (
     <PageLayout renderFooter={() => <h2>Hello from footer content</h2>}>
       <CenterLayout>
@@ -14,6 +24,12 @@ function IndexPage() {
           </li>
           <li>
             <Link to="test-page">Test page</Link>
+          </li>
+          <li>{title}</li>
+          <li>
+            <button type="button" onClick={handleButtonClick}>
+              Reset state
+            </button>
           </li>
         </ul>
       </CenterLayout>

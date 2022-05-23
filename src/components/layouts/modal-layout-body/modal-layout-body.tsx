756e96765890ecac8e6ad673029de2df02106364
pavel <shirokuiuna@mail.ru>
2022-05-23 15:23:16 +0300

    Отмена скрола контента под модалкой

--- src/components/layouts/modal-layout-body/modal-layout-body.tsx
+++ src/components/layouts/modal-layout-body/modal-layout-body.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import { useModalProps } from 'react-simple-modal-provider';
 
 import { Props } from '@layouts/modal-layout-body/types';
 
+import './modal-layout-body.scss';
+
 function ModalLayoutBody({ modalId }: Props) {
   const { component } = useModalProps(modalId);
+  const { documentElement } = document;
+
+  useEffect(() => {
+    documentElement.classList.add('modal-layout-body--modal-open');
+
+    return () => {
+      documentElement.classList.remove('modal-layout-body--modal-open');
+    };
+  }, []);
 
   return component;
 }

6efaaaa96122c88622e689d2a66a80e5a83ad666
pavelosipov <osipov_p_v@ntik.ru>
2022-06-06 23:58:39 +0300

    Поправил логику input-number

--- src/components/shared/input-number/types.ts
+++ src/components/shared/input-number/types.ts
@@ -5,7 +5,7 @@ export type Props = {
   placeholder?: string;
   min?: number;
   className?: string;
-  onChange?: (value: number) => void;
-  onBlur?: (value: number) => void;
-  onKeyDownEnter?: (value: number) => void;
+  onChange?: (value: string) => void;
+  onBlur?: (value: number | string) => void;
+  onKeyDownEnter?: (value: number | string) => void;
 };
